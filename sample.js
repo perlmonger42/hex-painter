56bcfe23ccbebdad62ebb8d5f857b429d0c3b059
Thom Boyer <thom@boyers.org>
2014-12-07 09:46:52 -0700

    Minor formatting changes. These were probably done to satisfy Crockford's jslint. [sample-004.js, 2012-05-06]

--- sample.js
+++ sample.js
@@ -4,17 +4,17 @@
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 function OnNoChange(callback, delayMilliseconds) {
-  this.restart = function () {
+  this.restart = function() {
     this.cancel();
     this.timeoutID = window.setTimeout(callback, delayMilliseconds);
   };
-  this.cancel = function () {
+  this.cancel = function() {
     if (typeof this.timeoutID == 'number') {
       window.clearTimeout(this.timeoutID);
       delete this.timeoutID;
     }
   };
-};
+}
 
 /*
     Possible textual representations of hex game states
@@ -38,19 +38,19 @@ $(document).ready(function() {
   var drawing = $('#draw');
   var code = $('#code');
 
-  var DrawOnNoChange = new OnNoChange(function ()
+  var DrawOnNoChange = new OnNoChange(function()
     {
       drawBoard(drawing, code[0].value);
     }, 100);
 
   DrawOnNoChange.restart();
-  code.css("border", "3px solid green", "font-family", "monospace");
+  code.css('border', '3px solid green', 'font-family', 'monospace');
   code.keyup(function() { DrawOnNoChange.restart() });
   $('#monitor').css('border', '2px solid blue', 'font-family', 'monospace');
 });
 
 function println(s) {
-  $('#monitor')[0].value += s + "\n";
+  $('#monitor')[0].value += s + '\n';
 }
 
 function drawBoard(where, board_description) {
@@ -60,7 +60,7 @@ function drawBoard(where, board_description) {
   var R = 20;        // major radius of hexagons
   var r = R * ratio; // minor radius of hexagons
   var dx = Math.round(r);    // delta-x for move-to's and line-to's
-  var dy = Math.round(R/2);  // delta-y for move-to's and line-to's
+  var dy = Math.round(R / 2);  // delta-y for move-to's and line-to's
   var margin = 5;
   var Cx = margin + dx, Cy = margin + 2 * dy, Row = 0;
 
@@ -71,6 +71,7 @@ function drawBoard(where, board_description) {
   var drawingWidth = 500, drawingHeight = 500;
   Paper = new Raphael(where[0], drawingWidth, drawingHeight);
   var drawingElements = Paper.set();
+  
 
   var tokens = tokenize(board_description);
   var maxCx = 0, maxCy = 0;
@@ -83,42 +84,42 @@ function drawBoard(where, board_description) {
       ++Row;
       Cy += 3 * dy;
       Cx = margin + dx;
-      println("Row #" + Row + " Cx:"+Cx+" Cy:"+Cy);
+      println('Row #' + Row + ' Cx:'+ Cx + ' Cy:'+ Cy);
     } else if (type == 'hex' && /_/.test(text)) {
-      Cx += 2*dx;
+      Cx += 2 * dx;
     } else if (type == 'hex' && /-/.test(text)) {
       Cx += dx;
     } else if (type == 'hex' && /\^/.test(text)) {
-      var path = 'M ' + (Cx-dx) + ' ' + (  Cy ) + ' '
-               + 'l ' + (   0 ) + ' ' + ( -dy ) + ' '
-               + 'l ' + (  dx ) + ' ' + ( -dy ) + ' '
-               + 'l ' + (  dx ) + ' ' + (  dy ) + ' '
-               + 'l ' + (   0 ) + ' ' + (  dy ) + ' '
-               + 'l ' + ( -dx ) + ' ' + ( -dy ) + ' z';
+      var path = 'M ' + (Cx - dx) + ' ' + (Cy) + ' '
+               + 'l ' + (0) + ' ' + (-dy) + ' '
+               + 'l ' + (dx) + ' ' + (-dy) + ' '
+               + 'l ' + (dx) + ' ' + (dy) + ' '
+               + 'l ' + (0) + ' ' + (dy) + ' '
+               + 'l ' + (-dx) + ' ' + (-dy) + ' z';
       Paper.path(path).attr({fill: 'red', stroke: 'none'});
-      Cx += 2*dx;
+      Cx += 2 * dx;
     } else if (type == 'hex' && /\\/.test(text)) {
-      var path = 'M ' + (Cx-dx/2 ) + ' ' + (Cy-3*dy/2) + ' '
-               + 'l ' + ( dx/2   ) + ' ' + ( -dy/2 ) + ' '
-               + 'l ' + (  dx ) + ' ' + (  dy ) + ' '
-               + 'l ' + (   0 ) + ' ' + (  dy ) + ' '
-               + 'l ' + ( -dx ) + ' ' + ( -dy ) + ' z';
+      var path = 'M ' + (Cx - dx / 2) + ' ' + (Cy - 3 * dy / 2) + ' '
+               + 'l ' + (dx / 2) + ' ' + (-dy / 2) + ' '
+               + 'l ' + (dx) + ' ' + (dy) + ' '
+               + 'l ' + (0) + ' ' + (dy) + ' '
+               + 'l ' + (-dx) + ' ' + (-dy) + ' z';
       Paper.path(path).attr({fill: 'red', stroke: 'none'});
-      Cx += 2*dx;
+      Cx += 2 * dx;
     } else if (type == 'hex' && /\//.test(text)) {
-      var path = 'M ' + (Cx+dx/2 ) + ' ' + (Cy-3*dy/2) + ' '
-               + 'l ' + ( -dx/2  ) + ' ' + ( -dy/2 ) + ' '
-               + 'l ' + (  -dx   ) + ' ' + (  dy ) + ' '
-               + 'l ' + (   0    ) + ' ' + (  dy ) + ' '
-               + 'l ' + (   dx   ) + ' ' + ( -dy ) + ' z';
+      var path = 'M ' + (Cx + dx / 2) + ' ' + (Cy - 3 * dy / 2) + ' '
+               + 'l ' + (-dx / 2) + ' ' + (-dy / 2) + ' '
+               + 'l ' + (-dx) + ' ' + (dy) + ' '
+               + 'l ' + (0) + ' ' + (dy) + ' '
+               + 'l ' + (dx) + ' ' + (-dy) + ' z';
       Paper.path(path).attr({fill: 'red', stroke: 'none'});
-      Cx += 2*dx;
+      Cx += 2 * dx;
     } else if (type == 'hex') {
       var wedge = '';
       var fill;
       var dash;
 
-      wedge  = '';
+      wedge = '';
       wedge += /u/.test(text) ? 'u' : /n/.test(text) ? 'n' : '';
       wedge += /b/.test(text) ? 'b' : /c/.test(text) ? 'c' : '';
 
@@ -132,15 +133,15 @@ function drawBoard(where, board_description) {
         fill = 'lightgray', dash = '.';
       }
 
-      var x=Cx, y=Cy;
+      var x = Cx, y = Cy;
       draw_hex(fill, dash, wedge);
       if (/[1]/.test(text)) {
-        Paper.circle(x, y, r/3).attr({fill:'red'});;
+        Paper.circle(x, y, r / 3).attr({fill: 'red'});
       }
     }
   }
-  Paper.setSize(maxCx + dx + margin, maxCy + 2*dy + margin);
-  where.width(maxCx + dx + margin).height(maxCy + 2*dy + margin);
+  Paper.setSize(maxCx + dx + margin, maxCy + 2 * dy + margin);
+  where.width(maxCx + dx + margin).height(maxCy + 2 * dy + margin);
 
   var s = '';
   var comma = '';
@@ -166,65 +167,65 @@ function drawBoard(where, board_description) {
     if (Cx > maxCx) maxCx = Cx;
     if (Cy > maxCy) maxCy = Cy;
     draw_hex_at(Cx, Cy, fill, stroke, wedge);
-    Cx += 2*dx;
+    Cx += 2 * dx;
   }
 
 
   function draw_hex_at(x, y, fill, stroke, wedge) {
     var poly, line;
     if (wedge == 'b') { // draw right half
-      line = 'M ' + (  x  ) + ' ' + ( y-2*dy ) + ' '
-           + 'l ' + (  dx ) + ' ' + (   dy   ) + ' '
-           + 'l ' + (  0  ) + ' ' + (  2*dy  ) + ' '
-           + 'l ' + ( -dx ) + ' ' + (   dy   );
+      line = 'M ' + (x) + ' ' + (y - 2 * dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy) + ' '
+           + 'l ' + (0) + ' ' + (2 * dy) + ' '
+           + 'l ' + (-dx) + ' ' + (dy);
       poly = line + ' z';
     } else if (wedge == 'c') { // draw left half
-      line = 'M ' + (  x  ) + ' ' + ( y-2*dy ) + ' '
-           + 'l ' + ( -dx ) + ' ' + (   dy   ) + ' '
-           + 'l ' + (  0  ) + ' ' + (  2*dy  ) + ' '
-           + 'l ' + (  dx ) + ' ' + (   dy   );
+      line = 'M ' + (x) + ' ' + (y - 2 * dy) + ' '
+           + 'l ' + (-dx) + ' ' + (dy) + ' '
+           + 'l ' + (0) + ' ' + (2 * dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy);
       poly = line + ' z';
     } else if (wedge == 'n') { // draw top half
-      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (  -dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (   dy  ) + ' '
-           + 'l ' + (  0   ) + ' ' + (   dy  );
+      line = 'M ' + (x - dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (-dy) + ' '
+           + 'l ' + (dx) + ' ' + (-dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy) + ' '
+           + 'l ' + (0) + ' ' + (dy);
       poly = line + ' z';
     } else if (wedge == 'u') { // draw bottom half
-      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (   dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (  -dy  ) + ' '
-           + 'l ' + (  0   ) + ' ' + (  -dy  );
+      line = 'M ' + (x - dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy) + ' '
+           + 'l ' + (dx) + ' ' + (-dy) + ' '
+           + 'l ' + (0) + ' ' + (-dy);
       poly = line + ' z';
     } else if (wedge == 'ub') { // draw bottom-right quarter
-      line = 'M ' + ( x+dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
-           + 'l ' + ( -dx  ) + ' ' + (   dy  );
-      poly = line + 'l ' + (  0   ) + ' ' + ( -2*dy ) + ' ' + ' z';
+      line = 'M ' + (x + dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (dy) + ' '
+           + 'l ' + (-dx) + ' ' + (dy);
+      poly = line + 'l ' + (0) + ' ' + (-2 * dy) + ' ' + ' z';
     } else if (wedge == 'uc') { // draw bottom-left quarter
-      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (   dy  );
-      poly = line + 'l ' + (  0   ) + ' ' + ( -2*dy ) + ' ' + ' z';
+      line = 'M ' + (x - dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy);
+      poly = line + 'l ' + (0) + ' ' + (-2 * dy) + ' ' + ' z';
     } else if (wedge == 'nb') { // draw top-right quarter
-      line = 'M ' + ( x+dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
-           + 'l ' + ( -dx  ) + ' ' + (  -dy  );
-      poly = line + 'l ' + (  0   ) + ' ' + (  2*dy ) + ' ' + ' z';
+      line = 'M ' + (x + dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (-dy) + ' '
+           + 'l ' + (-dx) + ' ' + (-dy);
+      poly = line + 'l ' + (0) + ' ' + (2 * dy) + ' ' + ' z';
     } else if (wedge == 'nc') { // draw top-left quarter
-      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
-           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
-           + 'l ' + (  dx  ) + ' ' + (  -dy  );
-      poly = line + 'l ' + (  0   ) + ' ' + (  2*dy ) + ' ' + ' z';
+      line = 'M ' + (x - dx) + ' ' + (y) + ' '
+           + 'l ' + (0) + ' ' + (-dy) + ' '
+           + 'l ' + (dx) + ' ' + (-dy);
+      poly = line + 'l ' + (0) + ' ' + (2 * dy) + ' ' + ' z';
     } else {
-      poly = 'M ' + ( x ) + ' ' + ( y+2*dy ) + ' '
-           + 'l ' + (-dx) + ' ' + (  -dy   ) + ' '
-           + 'l ' + ( 0 ) + ' ' + ( -2*dy  ) + ' '
-           + 'l ' + ( dx) + ' ' + (  -dy   ) + ' '
-           + 'l ' + ( dx) + ' ' + (   dy   ) + ' '
-           + 'l ' + ( 0 ) + ' ' + (  2*dy  ) + ' '
+      poly = 'M ' + (x) + ' ' + (y + 2 * dy) + ' '
+           + 'l ' + (-dx) + ' ' + (-dy) + ' '
+           + 'l ' + (0) + ' ' + (-2 * dy) + ' '
+           + 'l ' + (dx) + ' ' + (-dy) + ' '
+           + 'l ' + (dx) + ' ' + (dy) + ' '
+           + 'l ' + (0) + ' ' + (2 * dy) + ' '
            + 'z';
       Paper.path(poly).attr({fill: fill, 'stroke-dasharray': stroke});
       return;
@@ -244,7 +245,7 @@ function drawBoard(where, board_description) {
     //              \   /          |                 | 1/2 R
     //               \ /           |                 |
     //                +       <----+         <-------+
-    //                
+    //
     //            ^   ^   ^
     //            |   |   |
     //            +---+   |
@@ -263,7 +264,7 @@ function drawBoard(where, board_description) {
     var result = [];            // An array to hold the results.
 
     // Make a token object, like {type:_, text:_, from:_, to:_}
-    var make = function (type, text) {
+    var make = function(type, text) {
       return {
         type: type,
         text: text,
@@ -274,7 +275,7 @@ function drawBoard(where, board_description) {
 
     // Begin tokenization. If the source string is empty, return nothing.
     if (!source) {
-        return [ ];
+        return [];
     }
 
     // Loop through this text, one character at a time.
@@ -296,7 +297,7 @@ function drawBoard(where, board_description) {
         str = '';
         while (c && c > ' ' && c != 'b') {
           str += c, ++i, c = source.charAt(i);
-        } 
+        }
         if (c == 'b') {
           str += c, ++i, c = source.charAt(i);
         }
@@ -305,7 +306,7 @@ function drawBoard(where, board_description) {
         str = '';
         while (c && c > ' ' && c != 'c' && c != 'b') {
           str += c, ++i, c = source.charAt(i);
-        } 
+        }
         if (c == 'b') {
           str += c, ++i, c = source.charAt(i);
         }
