83268135672e01f74eb58370f6cb773b88ee4fdf
Thom Boyer <thom@boyers.org>
2014-12-07 09:43:10 -0700

    First actual hex boards. The contents of a textbox can be edited to create an SVG image of a hex board on the same page. [sample-003.html, 2012-06-10; sample-003.js, 2012-05-06]

--- sample.js
+++ sample.js
@@ -1,140 +1,319 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  This sample is based on
-    http://net.tutsplus.com/tutorials/javascript-ajax/an-introduction-to-the-raphael-js-library/
-  That source is a Raphael tutorial, and it does not use jQuery. I've modified
-  the tutorial to include jQuery. Also, that tutorial is based on version 1.0
-  of Raphael. I have changed it to work with Raphael 2.0, with help from 
-    http://stackoverflow.com/questions/8115713/drag-drop-and-shape-rotation-with-raphael-js
-  and the sample code that links to, at
-    http://jsfiddle.net/amadanNM/3DBWM/
-  and
-    http://jsfiddle.net/amadanNM/Xy4zp/
+  This is an attempt to create a page with a textbox, whose contents
+  can be edited to create an SVG image on the same page.
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+function OnNoChange(callback, delayMilliseconds) {
+  this.restart = function () {
+    this.cancel();
+    this.timeoutID = window.setTimeout(callback, delayMilliseconds);
+  };
+  this.cancel = function () {
+    if (typeof this.timeoutID == 'number') {
+      window.clearTimeout(this.timeoutID);
+      delete this.timeoutID;
+    }
+  };
+};
+
+/*
+    Possible textual representations of hex game states
+
+    > < > < > <*> <O> < > <
+    < > < > <O> <O> <O> < >
+    >< ><O><O><O><O><
+
+    >. . * O .<
+    . . O O O .
+    >. O O O O<
+    _ _ ^ ^ ^ _
+
+    .. . * O . .
+    . . O O O ..
+    .. O O O O .
+       = = = =
+*/
+
 $(document).ready(function() {
-  drawRaph();
-  drawRaph2();
+  var drawing = $('#draw');
+  var code = $('#code');
+
+  var DrawOnNoChange = new OnNoChange(function ()
+    {
+      drawBoard(drawing, code[0].value);
+    }, 100);
+
+  DrawOnNoChange.restart();
+  code.css("border", "3px solid green", "font-family", "monospace");
+  code.keyup(function() { DrawOnNoChange.restart() });
+  $('#monitor').css('border', '2px solid blue', 'font-family', 'monospace');
 });
 
-function drawRaph() {
-  var raph = $('#raph');
-  raph.width(500).height(500).css("border", "3px solid green");
-  var paper = new Raphael(raph[0], 500, 500);
-  // Upper left corner of the canvas is (0,0), with x values increasing
-  // rightward and y values increasing downward.
-  
-  /* circle args: center-x, center-y, radius */
-  paper.circle(100, 100, 80);
-  for (var i = 0; i < 5; ++i) {
-    var multiplier = i * 5;
-    paper.circle(250 + (2*multiplier), 100 + multiplier, 50 - multiplier);
-  }
-  /* rect args: left-x, top-y, width, height */
-  paper.rect(275, 200, 200, 100);
-  /* ellipse args: center-x, center-y, horizontal-radius, vertical-radius */
-  paper.ellipse(200, 400, 100, 50);
-
-  var tetronimo_draw = "l 0 -50 " +
-                       "l -50 0 " +
-                       "l 0 -50 " +
-                       "l -50 0 " +
-                       "l 0 50 " +
-                       "l -50 0 " +
-                       "l 0 50 " +
-                       "z";
-  var tetronimo1 = paper.path("M 250 250 " + tetronimo_draw);
-  tetronimo1.attr({fill: '#9cf', stroke: '#ddd', 'stroke-width': 5}); 
-  tetronimo1.animate({ transform: "...T-100,0" }, 2000, "bounce");
-  var tetronimo2 = paper.path("M 250 400 " + tetronimo_draw);
-  tetronimo2.attr(  
-    {  
-      gradient: '90-#526c7a-#64a0c1',  
-      stroke: '#3b4449',  
-      'stroke-width': 10,  
-      'stroke-linejoin': 'round',  
-      transform: 'r270'
-    }  
-  );  
-  tetronimo2.animate({
-    transform: "...R-355",
-    stroke: '#fa0',  
-    'stroke-width': 1,
-  }, 1000, "<>", function () {
-    this.animate({
-      transform: "...+R355",
-      stroke: '#3b4449',  
-      'stroke-width': 10,
-      transform: 'r270'
-    }, 1000, "<>", function () {
-      this.animate(
-        {
-          path: "M 250 250 l 0 -50 l -50 0 l 0 -50 " +
-                "l -100 0 l 0 50 l 50 0 l 0 50 z"  
-        }, 5000, 'elastic'
-      );
-    })
-  });
-
-  tetronimo2.mouseover(function() {
-    this.attr('cursor', 'crosshair');
-  });
-  tetronimo1.click(function () {
-    this.animate({opacity: 0}, 2000, "<>", function() { this.remove() } );
-  });
+function println(s) {
+  $('#monitor')[0].value += s + "\n";
 }
 
-function drawRaph2() {
-  var raph = $('#raph2');
-  raph.width(200).height(500).css("border", "3px solid red");
-  var paper = new Raphael(raph[0], 200, 500);
+function drawBoard(where, board_description) {
+  if (typeof(board_description) == 'undefined')
+    return;
+  var ratio = 0.866; // minor radius is sqrt(3/4) times the major radius
+  var R = 20;        // major radius of hexagons
+  var r = R * ratio; // minor radius of hexagons
+  var dx = Math.round(r);    // delta-x for move-to's and line-to's
+  var dy = Math.round(R/2);  // delta-y for move-to's and line-to's
+  var margin = 5;
+  var Cx = margin + dx, Cy = margin + 2 * dy, Row = 0;
 
-  //pick a mood between 1 and 5, 1 being rubbish and 5 being positively manic  
-  var my_mood = 5;
+  where.children().remove();
+  where.css('border', '2px solid orange');
+  $('#monitor')[0].value = '';
 
-  var moods = ['Rubbish', 'Not Good', 'OK', 'Smily', 'Positively Manic'];  
-  var colors = ['#cc0000', '#a97e22', '#9f9136', '#7c9a2d', '#3a9a2d'];  
-  var circ = paper.circle(100, 250, 20).attr({fill: '#000'})
-  var circ_label = paper.text(100, 250, 'My\nMood').attr({fill: '#fff'});
-  var circles = [ ];
-  var mood_text;
+  var drawingWidth = 500, drawingHeight = 500;
+  Paper = new Raphael(where[0], drawingWidth, drawingHeight);
+  var drawingElements = Paper.set();
 
-  var show_mood = function() {
-    var do_nothing = function () { return false; };
-    circ.click(do_nothing);  
-    circ_label.click(do_nothing);
+  var tokens = tokenize(board_description);
+  var maxCx = 0, maxCy = 0;
+  for (var i = 0; i < tokens.length; ++i) {
+    var token = tokens[i];
+    var type = token.type;
+    var text = token.text;
 
-    mood_text = paper.text(100, 300, moods[my_mood - 1]);
-    mood_text.attr({fill: colors[my_mood - 1]});  
+    if (type == 'newline') {
+      ++Row;
+      Cy += 3 * dy;
+      Cx = margin + dx;
+      println("Row #" + Row + " Cx:"+Cx+" Cy:"+Cy);
+    } else if (type == 'hex' && /_/.test(text)) {
+      Cx += 2*dx;
+    } else if (type == 'hex' && /-/.test(text)) {
+      Cx += dx;
+    } else if (type == 'hex' && /\^/.test(text)) {
+      var path = 'M ' + (Cx-dx) + ' ' + (  Cy ) + ' '
+               + 'l ' + (   0 ) + ' ' + ( -dy ) + ' '
+               + 'l ' + (  dx ) + ' ' + ( -dy ) + ' '
+               + 'l ' + (  dx ) + ' ' + (  dy ) + ' '
+               + 'l ' + (   0 ) + ' ' + (  dy ) + ' '
+               + 'l ' + ( -dx ) + ' ' + ( -dy ) + ' z';
+      Paper.path(path).attr({fill: 'red', stroke: 'none'});
+      Cx += 2*dx;
+    } else if (type == 'hex' && /\\/.test(text)) {
+      var path = 'M ' + (Cx-dx/2 ) + ' ' + (Cy-3*dy/2) + ' '
+               + 'l ' + ( dx/2   ) + ' ' + ( -dy/2 ) + ' '
+               + 'l ' + (  dx ) + ' ' + (  dy ) + ' '
+               + 'l ' + (   0 ) + ' ' + (  dy ) + ' '
+               + 'l ' + ( -dx ) + ' ' + ( -dy ) + ' z';
+      Paper.path(path).attr({fill: 'red', stroke: 'none'});
+      Cx += 2*dx;
+    } else if (type == 'hex' && /\//.test(text)) {
+      var path = 'M ' + (Cx+dx/2 ) + ' ' + (Cy-3*dy/2) + ' '
+               + 'l ' + ( -dx/2  ) + ' ' + ( -dy/2 ) + ' '
+               + 'l ' + (  -dx   ) + ' ' + (  dy ) + ' '
+               + 'l ' + (   0    ) + ' ' + (  dy ) + ' '
+               + 'l ' + (   dx   ) + ' ' + ( -dy ) + ' z';
+      Paper.path(path).attr({fill: 'red', stroke: 'none'});
+      Cx += 2*dx;
+    } else if (type == 'hex') {
+      var wedge = '';
+      var fill;
+      var dash;
 
-    for(var i = 0; i < my_mood; i+=1) {  
-      (function(i) {  
-        setTimeout(function() {  
-          var c = paper.circle(100, 250, 20);
-          c.attr({  stroke: 'none',  fill: colors[my_mood - 1]  });
-          c.animate({ transform: 'T0 ' + (-42 * (i+1)) }, 2000, '<>');
-          c.toBack();  
-          circles[i] = c;
-        }, 50*i);  
-      })(i);  
-    }  
+      wedge  = '';
+      wedge += /u/.test(text) ? 'u' : /n/.test(text) ? 'n' : '';
+      wedge += /b/.test(text) ? 'b' : /c/.test(text) ? 'c' : '';
 
-    var lower_my_mood = function() {
-      if (my_mood > 1) {
-        for (var i = 0; i < my_mood; ++i) {
-          circles[i].remove();
-        }
-        mood_text.remove();
-        --my_mood;
-        show_mood();
+      if (/[*]/.test(text)) {
+        fill = 'red', dash = '';
+      } else if (/[O]/.test(text)) {
+        fill = 'white', dash = '';
+      } else if (/[01]/.test(text)) {
+        fill = 'yellow', dash = '';
+      } else {
+        fill = 'lightgray', dash = '.';
       }
-    };
 
-    setTimeout(lower_my_mood, 3000);
+      var x=Cx, y=Cy;
+      draw_hex(fill, dash, wedge);
+      if (/[1]/.test(text)) {
+        Paper.circle(x, y, r/3).attr({fill:'red'});;
+      }
+    }
+  }
+  Paper.setSize(maxCx + dx + margin, maxCy + 2*dy + margin);
+  where.width(maxCx + dx + margin).height(maxCy + 2*dy + margin);
 
+  var s = '';
+  var comma = '';
+  for (var i = 0; i < tokens.length; ++i) {
+    var t = tokens[i];
+    if (t.type == 'newline') {
+      println(s); s = ''; comma = '';
+    } else {
+      s += comma + '[' + t.type + ': ' + t.text + ']';
+      comma = ', ';
+    }
+  }
+  if (s != '') {
+    println(s);
   }
 
-  circ.click(show_mood);
-  circ_label.click(show_mood);
-  
+  return;
+
+  function draw_hex(fill, stroke, wedge) {
+    if (/b/.test(wedge)) {
+      Cx -= dx;
+    }
+    if (Cx > maxCx) maxCx = Cx;
+    if (Cy > maxCy) maxCy = Cy;
+    draw_hex_at(Cx, Cy, fill, stroke, wedge);
+    Cx += 2*dx;
+  }
+
+
+  function draw_hex_at(x, y, fill, stroke, wedge) {
+    var poly, line;
+    if (wedge == 'b') { // draw right half
+      line = 'M ' + (  x  ) + ' ' + ( y-2*dy ) + ' '
+           + 'l ' + (  dx ) + ' ' + (   dy   ) + ' '
+           + 'l ' + (  0  ) + ' ' + (  2*dy  ) + ' '
+           + 'l ' + ( -dx ) + ' ' + (   dy   );
+      poly = line + ' z';
+    } else if (wedge == 'c') { // draw left half
+      line = 'M ' + (  x  ) + ' ' + ( y-2*dy ) + ' '
+           + 'l ' + ( -dx ) + ' ' + (   dy   ) + ' '
+           + 'l ' + (  0  ) + ' ' + (  2*dy  ) + ' '
+           + 'l ' + (  dx ) + ' ' + (   dy   );
+      poly = line + ' z';
+    } else if (wedge == 'n') { // draw top half
+      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (  -dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (   dy  ) + ' '
+           + 'l ' + (  0   ) + ' ' + (   dy  );
+      poly = line + ' z';
+    } else if (wedge == 'u') { // draw bottom half
+      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (   dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (  -dy  ) + ' '
+           + 'l ' + (  0   ) + ' ' + (  -dy  );
+      poly = line + ' z';
+    } else if (wedge == 'ub') { // draw bottom-right quarter
+      line = 'M ' + ( x+dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
+           + 'l ' + ( -dx  ) + ' ' + (   dy  );
+      poly = line + 'l ' + (  0   ) + ' ' + ( -2*dy ) + ' ' + ' z';
+    } else if (wedge == 'uc') { // draw bottom-left quarter
+      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (   dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (   dy  );
+      poly = line + 'l ' + (  0   ) + ' ' + ( -2*dy ) + ' ' + ' z';
+    } else if (wedge == 'nb') { // draw top-right quarter
+      line = 'M ' + ( x+dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
+           + 'l ' + ( -dx  ) + ' ' + (  -dy  );
+      poly = line + 'l ' + (  0   ) + ' ' + (  2*dy ) + ' ' + ' z';
+    } else if (wedge == 'nc') { // draw top-left quarter
+      line = 'M ' + ( x-dx ) + ' ' + (   y   ) + ' '
+           + 'l ' + (  0   ) + ' ' + (  -dy  ) + ' '
+           + 'l ' + (  dx  ) + ' ' + (  -dy  );
+      poly = line + 'l ' + (  0   ) + ' ' + (  2*dy ) + ' ' + ' z';
+    } else {
+      poly = 'M ' + ( x ) + ' ' + ( y+2*dy ) + ' '
+           + 'l ' + (-dx) + ' ' + (  -dy   ) + ' '
+           + 'l ' + ( 0 ) + ' ' + ( -2*dy  ) + ' '
+           + 'l ' + ( dx) + ' ' + (  -dy   ) + ' '
+           + 'l ' + ( dx) + ' ' + (   dy   ) + ' '
+           + 'l ' + ( 0 ) + ' ' + (  2*dy  ) + ' '
+           + 'z';
+      Paper.path(poly).attr({fill: fill, 'stroke-dasharray': stroke});
+      return;
+    }
+    Paper.path(poly).attr({fill: fill, stroke: 'none'});
+    Paper.path(line).attr({'stroke-dasharray': stroke});
+    //                +                      <---+
+    //               / \                         |
+    //              /   \                        | 1/2 R
+    //             /     \                       |
+    //            +       +                  <---+-+
+    //            |       |                        |
+    //            |   o   |    <---+               | R
+    //            |       |        |               |
+    //            +       +        |         <-----+-+
+    //             \     /         | R               |
+    //              \   /          |                 | 1/2 R
+    //               \ /           |                 |
+    //                +       <----+         <-------+
+    //                
+    //            ^   ^   ^
+    //            |   |   |
+    //            +---+   |
+    //              r +---+
+    //                  r
+  }
+
+  // Break the string {source} into tokens and return the list of tokens.
+  // This function is based on http://javascript.crockford.com/tdop/tokens.js
+  function tokenize(source) {
+    var c;                      // The current character.
+    var from;                   // The index of the start of the token.
+    var i = 0;                  // The index of the current character.
+    var str;                    // The string value.
+
+    var result = [];            // An array to hold the results.
+
+    // Make a token object, like {type:_, text:_, from:_, to:_}
+    var make = function (type, text) {
+      return {
+        type: type,
+        text: text,
+        from: from,
+        to: i
+      };
+    };
+
+    // Begin tokenization. If the source string is empty, return nothing.
+    if (!source) {
+        return [ ];
+    }
+
+    // Loop through this text, one character at a time.
+    c = source.charAt(i);
+    while (c) {
+      from = i;
+
+      if (c == '\r' || c == '\n') {
+        // Newline
+        str = c, ++i, c = source.charAt(i);
+        if (str == '\r' && c == '\n') {
+          str += c, ++i, c = source.charAt(i);
+        }
+        result.push(make('newline', str));
+      } else if (c <= ' ') {
+        // Ignore whitespace.
+        ++i, c = source.charAt(i);
+      } else if (c == 'c') {
+        str = '';
+        while (c && c > ' ' && c != 'b') {
+          str += c, ++i, c = source.charAt(i);
+        } 
+        if (c == 'b') {
+          str += c, ++i, c = source.charAt(i);
+        }
+        result.push(make('hex', str));
+      } else {
+        str = '';
+        while (c && c > ' ' && c != 'c' && c != 'b') {
+          str += c, ++i, c = source.charAt(i);
+        } 
+        if (c == 'b') {
+          str += c, ++i, c = source.charAt(i);
+        }
+        result.push(make('hex', str));
+      }
+    }
+    return result;
+  };
 
 }
 
